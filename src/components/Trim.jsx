ba0bd5afe133128b7062825a6a91a04f8ec5026a
bezbrain <belloquadriolawale@gmail.com>
2023-07-04 06:47:01 -0700

    Completed the UI of the URL Pasting section

---
+++ src/components/Trim.jsx
@@ -0,0 +1,28 @@
+import "../styles/trim.css";
+
+const Trim = () => {
+  return (
+    <main className="trim">
+      <form>
+        <input type="text" placeholder="Paste original URL here" />
+        <div className="change-act">
+          <select name="" id="">
+            <option value="">Choose </option>
+            <option value="Trim">Trim</option>
+            <option value="Customize">Customize</option>
+          </select>
+          <input type="text" placeholder="About to Trim" />
+        </div>
+        <button>Trim URL</button>
+        <br />
+        <input type="text" placeholder="Get generated URL here" />
+        <p>
+          By clicking Trim URL, I agree with the Terms of Service, Privacy
+          Policy and Use of Cookies
+        </p>
+      </form>
+    </main>
+  );
+};
+
+export default Trim;
--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -2,17 +2,19 @@ import { Link } from "react-router-dom";
 import HeroSection from "../components/HeroSection";
 import Header from "../components/Header";
 import WhyChooseUs from "../components/WhyChooseUs";
+import Trim from "../components/Trim";
 
 const LandingPage = () => {
   return (
     <>
       <main className="header-and-hero">
         <HeroSection />
+        <div className="hero-img-con">
+          <img src="../../Images/HeroSection_img.jpeg" alt="" />
+        </div>
       </main>
-      <div className="hero-img-con">
-        <img src="../../Images/HeroSection_img.jpeg" alt="" />
-      </div>
       <WhyChooseUs />
+      <Trim />
     </>
   );
 };
