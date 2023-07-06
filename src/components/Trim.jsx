0b609a4ba7f9cb9a9ececac08077f669b27ff791
bezbrain <belloquadriolawale@gmail.com>
2023-07-06 08:27:50 -0700

    Collects inputs and select values in the Trim section

--- src/components/Trim.jsx
+++ src/components/Trim.jsx
@@ -1,4 +1,6 @@
+import { useRef } from "react";
 import { useGlobalContext } from "../context";
+import { useNavigate } from "react-router-dom";
 import "../styles/trim.css";
 
 const Trim = () => {
@@ -13,6 +15,10 @@ const Trim = () => {
     setTrimResult,
   } = useGlobalContext();
 
+  const navigate = useNavigate();
+
+  const buttonTextContentRef = useRef(null);
+
   const getToken = localStorage.getItem("authToken");
   // console.log(getToken);
 
@@ -23,11 +29,54 @@ const Trim = () => {
       ...trimType,
       [name]: value,
     });
-    console.log(trimType);
+    // console.log(trimType);
   };
 
   const handleTrimBtn = (e) => {
     e.preventDefault();
+
+    let buttonTextContent = buttonTextContentRef.current.textContent;
+
+    // Check if logged in
+    if (getToken) {
+      if (!originalUrl) {
+        console.log("Pls enter original URL");
+        return;
+      }
+      if (buttonTextContent === "Choose Trim-type") {
+        console.log("Pls choose a Trim type");
+        return;
+      }
+      if (trimType.TrimType === "Customize" && !customizeUrl) {
+        console.log("Pls, type preferred URL");
+        return;
+      }
+      if (trimType.TrimType === "Trim") {
+        console.log(trimURLFunc());
+        setTrimResult(trimURLFunc());
+        return;
+      }
+      console.log("Write code to input customized or trimmed URL here");
+    } else {
+      // Check if logged out
+      console.log("Please, Login to use service");
+    }
+  };
+
+  // Function to Trim URL
+  const trimURLFunc = () => {
+    let newOriginalUrl;
+    if (originalUrl.length <= 100) {
+      newOriginalUrl = originalUrl.slice(0, 30);
+      return newOriginalUrl;
+    }
+    newOriginalUrl = originalUrl.slice(0, 40);
+    return newOriginalUrl;
+  };
+
+  // Function to Customize URL
+  const customizeURLFunc = () => {
+    //
   };
 
   return (
@@ -41,7 +90,7 @@ const Trim = () => {
         />
         <div className="change-act">
           <select name="TrimType" id="TrimType" onChange={handleTrimTypeChange}>
-            <option value="">Choose </option>
+            <option value="">Choose</option>
             <option value="Trim">Trim</option>
             <option value="Customize">Customize</option>
           </select>
@@ -50,13 +99,13 @@ const Trim = () => {
             placeholder={`${
               trimType.TrimType === "Customize"
                 ? "Type preferred URL"
-                : "About to Trim"
+                : "About to Trim..."
             }`}
             value={`${trimType.TrimType === "Customize" ? customizeUrl : ""}`}
             onChange={(e) => setCustomizeUrl(e.target.value)}
           />
         </div>
-        <button onClick={handleTrimBtn}>{`${
+        <button onClick={handleTrimBtn} ref={buttonTextContentRef}>{`${
           trimType.TrimType === "Trim"
             ? "Trim URL"
             : trimType.TrimType === "Customize"
@@ -64,10 +113,18 @@ const Trim = () => {
             : "Choose Trim-type"
         }`}</button>
         <br />
-        <input type="text" placeholder="Get generated URL here" />
+        <input
+          type="text"
+          placeholder="Get generated URL here"
+          value={trimResult}
+          onChange={(e) => setTrimResult(e.target.value)}
+        />
         <p>
-          By clicking Trim URL, I agree with the Terms of Service, Privacy
-          Policy and Use of Cookies
+          By clicking{" "}
+          <span>{`${
+            trimType.TrimType === "Customize" ? "Customize URL" : "Trim URL"
+          }`}</span>
+          , I agree with the Terms of Service, Privacy Policy and Use of Cookies
         </p>
       </form>
     </main>
